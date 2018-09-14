d75eeb79ae5a46695b988daea33b66ffd4b2f9ca
sheilambadi <mbadiatieno@gmail.com>
2018-09-14 15:51:02 +0300

    Generate entities

---
+++ src/main/java/com/sheilambadi/entities/Exam.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sheilambadi.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author sheilambadi
+ */
+@Entity
+@Table(name = "exam")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Exam.findAll", query = "SELECT e FROM Exam e")
+    , @NamedQuery(name = "Exam.findById", query = "SELECT e FROM Exam e WHERE e.id = :id")
+    , @NamedQuery(name = "Exam.findByExamName", query = "SELECT e FROM Exam e WHERE e.examName = :examName")})
+public class Exam implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "exam_name")
+    private String examName;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "examId")
+    private List<Result> resultList;
+
+    public Exam() {
+    }
+
+    public Exam(Integer id) {
+        this.id = id;
+    }
+
+    public Exam(Integer id, String examName) {
+        this.id = id;
+        this.examName = examName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getExamName() {
+        return examName;
+    }
+
+    public void setExamName(String examName) {
+        this.examName = examName;
+    }
+
+    @XmlTransient
+    public List<Result> getResultList() {
+        return resultList;
+    }
+
+    public void setResultList(List<Result> resultList) {
+        this.resultList = resultList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Exam)) {
+            return false;
+        }
+        Exam other = (Exam) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.sheilambadi.entities.Exam[ id=" + id + " ]";
+    }
+    
+}
